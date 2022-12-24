92c9d3ced964a74cf0c5ad50d36384a1f28af602
liyigang <liyigang@bytedance.com>
2022-12-24 22:17:39 +0800

    fix: dispatch

--- packages/react/src/currentDispatcher.ts
+++ packages/react/src/currentDispatcher.ts
@@ -1,11 +1,11 @@
 import { Action } from 'shared/ReactTypes';
 
 export type Dispatcher = {
-	useState: <T>(initialState: (() => T) | T) => [T, Disptach<T>];
+	useState: <T>(initialState: (() => T) | T) => [T, Dispatch<T>];
 	useEffect: (callback: (() => void) | void, deps: any[] | void) => void;
 };
 
-export type Disptach<State> = (action: Action<State>) => void;
+export type Dispatch<State> = (action: Action<State>) => void;
 
 const currentDispatcher: { current: null | Dispatcher } = {
 	current: null
